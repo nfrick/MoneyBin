a4ce8db7a8284350cc8e1d1389e9325057650eef
Nelson Frick <nfrick@gmail.com>
2017-09-13 23:27:40 -0300

    MoneyBin - fixed bug in frmExport (Excel exporting all records regardless the option); added table format and first row freeze.

--- MoneyBin/Forms/frmExport.cs
+++ MoneyBin/Forms/frmExport.cs
@@ -8,6 +8,7 @@ using System.Threading;
 using System.Windows.Forms;
 using System.Xml.Linq;
 using OfficeOpenXml;
+using OfficeOpenXml.Table;
 
 namespace MoneyBin {
     public partial class frmExport : Form {
@@ -109,53 +110,61 @@ namespace MoneyBin {
         private void ExportToExcel() {
             ToExcel(Items);
         }
-        
+
         private void ExportToAcertos() {
             ToExcel(MoneyBinDB.GetAcertoItems());
         }
 
         private void ToExcel(ICollection<BalanceItem> mItems) {
-                    var pck = new ExcelPackage(new FileInfo(textBoxSaveAs.Text));
-                    var ws = pck.Workbook.Worksheets.Add("Balance");
-                    ws.View.ShowGridLines = false;
+            var pck = new ExcelPackage(new FileInfo(textBoxSaveAs.Text));
+            var ws = pck.Workbook.Worksheets.Add("Balance");
+            ws.View.ShowGridLines = false;
 
-                    var col = 1;
-                    ws.Cells[1, col++].Value = "Banco";
-                    ws.Cells[1, col++].Value = "Data";
-                    ws.Cells[1, col++].Value = "Histórico";
-                    ws.Cells[1, col++].Value = "Documento";
-                    ws.Cells[1, col++].Value = "Valor";
-                    ws.Cells[1, col++].Value = "Afeta Saldo";
-                    // ws.Cells[1, col++].Value = "Saldo";
-                    ws.Cells[1, col++].Value = "Grupo";
-                    ws.Cells[1, col++].Value = "Categoria";
-                    ws.Cells[1, col++].Value = "SubCategoria";
-                    ws.Cells[1, col++].Value = "Descrição";
+            var col = 1;
+            ws.Cells[1, col++].Value = "Banco";
+            ws.Cells[1, col++].Value = "Data";
+            ws.Cells[1, col++].Value = "Histórico";
+            ws.Cells[1, col++].Value = "Documento";
+            ws.Cells[1, col++].Value = "Valor";
+            ws.Cells[1, col++].Value = "Afeta Saldo";
+            // ws.Cells[1, col++].Value = "Saldo";
+            ws.Cells[1, col++].Value = "Grupo";
+            ws.Cells[1, col++].Value = "Categoria";
+            ws.Cells[1, col++].Value = "SubCategoria";
+            ws.Cells[1, col++].Value = "Descrição";
 
-                    var row = 2;
-                    foreach (var item in Items) {
-                        col = 1;
-                        ws.Cells[row, col++].Value = item.Banco;
-                        ws.Cells[row, col++].Value = item.Data;
-                        ws.Cells[row, col++].Value = item.Historico;
-                        ws.Cells[row, col++].Value = item.Documento;
-                        ws.Cells[row, col++].Value = item.Valor;
-                        ws.Cells[row, col++].Value = item.AfetaSaldo;
-                        // ws.Cells[row, col++].Value = item.Saldo;
-                        ws.Cells[row, col++].Value = item.Grupo;
-                        ws.Cells[row, col++].Value = item.Categoria;
-                        ws.Cells[row, col++].Value = item.SubCategoria;
-                        ws.Cells[row++, col++].Value = item.Descricao;
-                    }
+            var row = 2;
+            foreach (var item in mItems) {
+                col = 1;
+                ws.Cells[row, col++].Value = item.Banco;
+                ws.Cells[row, col++].Value = item.Data;
+                ws.Cells[row, col++].Value = item.Historico;
+                ws.Cells[row, col++].Value = item.Documento;
+                ws.Cells[row, col++].Value = item.Valor;
+                ws.Cells[row, col++].Value = item.AfetaSaldo;
+                // ws.Cells[row, col++].Value = item.Saldo;
+                ws.Cells[row, col++].Value = item.Grupo;
+                ws.Cells[row, col++].Value = item.Categoria;
+                ws.Cells[row, col++].Value = item.SubCategoria;
+                ws.Cells[row++, col++].Value = item.Descricao;
+            }
 
-                    row--;
-                    ws.Cells[$"B2:B{row}"].Style.Numberformat.Format = "dd-MM-yyyy";
-                    ws.Cells[$"D2:D{row}"].Style.Numberformat.Format = "@";
-                    ws.Cells[$"E2:E{row}"].Style.Numberformat.Format = "#,##0.00";
-                    // ws.Cells[$"G2:G{row}"].Style.Numberformat.Format = "#,##0.00";
-                    ws.Cells.AutoFitColumns(0);
-                    pck.Save();
-                    MessageBox.Show(@"Data exported.", @"Export", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            row--;
+            ws.Cells[$"B2:B{row}"].Style.Numberformat.Format = "dd-MM-yyyy";
+            ws.Cells[$"D2:D{row}"].Style.Numberformat.Format = "@";
+            ws.Cells[$"E2:E{row}"].Style.Numberformat.Format = "#,##0.00";
+            // ws.Cells[$"G2:G{row}"].Style.Numberformat.Format = "#,##0.00";
+            ws.Cells.AutoFitColumns(0);
+
+            var range = ws.Cells[1, 1, row, col];
+            var table = ws.Tables.Add(range, "table1");
+            table.ShowTotal = true;
+            table.TableStyle = TableStyles.Light1;
+
+            ws.View.FreezePanes(2, 1);
+
+            pck.Save();
+            MessageBox.Show(@"Data exported.", @"Export", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
